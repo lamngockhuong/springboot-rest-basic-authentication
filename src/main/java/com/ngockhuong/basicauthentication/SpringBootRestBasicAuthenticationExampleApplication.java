82fef3dc817581db62825fe98683f07a969dc5ed
Lam Ngoc Khuong <me@ngockhuong.com>
2018-11-17 16:57:35 +0700

    init spring boot project

---
+++ src/main/java/com/ngockhuong/basicauthentication/SpringBootRestBasicAuthenticationExampleApplication.java
@@ -0,0 +1,12 @@
+package com.ngockhuong.basicauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestBasicAuthenticationExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestBasicAuthenticationExampleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ngockhuong/basicauthentication/SpringBootRestBasicAuthenticationExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ngockhuong.basicauthentication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootRestBasicAuthenticationExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
